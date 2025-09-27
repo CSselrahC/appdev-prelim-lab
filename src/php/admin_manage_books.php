0a02d61f07334854efb49eda04ced930c1fc7305
TianDevs <carandangchristianraphael@gmail.com>
2025-09-27 15:32:35 +0800

    3:32pm

--- src/php/admin_manage_books.php
+++ src/php/admin_manage_books.php
@@ -1,1 +1,8 @@
-sd
+<?php
+$mysqli = new mysqli("db", "root", "rootpassword", "library-db");
+
+if ($mysqli->connect_error) {
+    die("Connection failed: " . $mysqli->connect_error);
+}
+/* Carandang */
+?>
