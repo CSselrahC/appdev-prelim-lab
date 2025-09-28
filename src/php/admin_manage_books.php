2f91109ab63330055c6c031e30a7d62e3189dff7
TianDevs <carandangchristianraphael@gmail.com>
2025-09-28 12:05:18 +0800

    testing

--- src/php/admin_manage_books.php
+++ src/php/admin_manage_books.php
@@ -70,6 +70,7 @@
     $mysqli->close();
     ?>
 
+    <li><a href="../admin/admin.html" id="adminBtn">Go Back</a></li>
     <footer>
         <p>Simple Library Management System</p>
         <p>CCS112 - Applications Development and Emerging Technologies</p>
