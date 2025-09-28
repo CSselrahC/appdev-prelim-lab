a443b3033977b8a8a15c3770d766cfc0e0955f9d
TianDevs <carandangchristianraphael@gmail.com>
2025-09-28 12:55:07 +0800

    updated manage book

--- src/php/admin_manage_books.php
+++ src/php/admin_manage_books.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Library MS</title>
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+</head>
+<body>
+    <header>
+        <h1>Library Management System</h1>
+    </header>
+    <br>
+
 <?php
 $mysqli = new mysqli("db", "root", "rootpassword", "library-db");
 
